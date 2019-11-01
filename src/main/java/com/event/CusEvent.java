e5c0375d4d6c8562697e40d7730a91aeb1373484
Liuxd <Yc313223yc>
2019-11-01 17:56:39 +0800

    个人更新

---
+++ src/main/java/com/event/CusEvent.java
@@ -0,0 +1,30 @@
+package com.event;
+
+/**
+ * @author liuxd
+ * @version 1.0
+ * @date 2019-11-01 17:50
+ */
+import java.util.EventObject;
+
+/**
+ * 事件类,用于封装事件源及一些与事件相关的参数.
+ * @author Eric
+ */
+public class CusEvent extends EventObject {
+    private static final long serialVersionUID = 1L;
+    private Object source;//事件源
+
+    public CusEvent(Object source){
+        super(source);
+        this.source = source;
+    }
+
+    public Object getSource() {
+        return source;
+    }
+
+    public void setSource(Object source) {
+        this.source = source;
+    }
+}
---
+++ src/main/java/com/event/MainTest.java
@@ -0,0 +1,25 @@
+package com.event;
+
+/**
+ * @author liuxd
+ * @version 1.0
+ * @date 2019-11-01 17:54
+ */
+public class MainTest {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        EventSourceObject object = new EventSourceObject();
+        //注册监听器
+        object.addCusListener(new CusEventListener(){
+            @Override
+            public void fireCusEvent(CusEvent e) {
+                super.fireCusEvent(e);
+            }
+        });
+        //触发事件
+        object.setName("eric");
+    }
+}
